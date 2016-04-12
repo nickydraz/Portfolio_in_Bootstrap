112739383f4585e2ad8268440e7f5439f6bd5325
nickydraz <nickydraz@gmail.com>
2016-04-12 08:56:04 -0500

    "Initialize Repo for test build of new portfolio site"

---
+++ includes/style.php
@@ -0,0 +1,5 @@
+<!--Include needed styles -->
+<link rel="stylesheet" type="text/css" href="./boot/css/bootstrap.min.css">
+<link rel="stylesheet" type='text/css' href="./boot/js/bootstrap.min.js">
+<link rel="stylesheet" type="text/css" href="./boot/fonts/glyphicons-halflings-regular.svg">
+<link rel="stylesheet" type="text/css" href="./css/custom.css">
---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <? include 'includes/style.php'; ?>
+        <title>Home | Nicholas Drazenovic</title>
+</head>
+
+<body>
+    <!-- Begin header and navbar for page -->
+    <header class="navbar navbar-fixed-top navbar-inverse">
+        <div class="container">
+            <a id="logo" href='#'>Nicholas Drazenovic</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href='#'>Home</a>
+                    </li>
+                    <li>
+                        <a href='#'>Courses</a>
+                    </li>
+                    <li>
+                        <a href='#'>About</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- End navbar -->
+    <!-- Begin main content -->
+    <div class='center middle jumbotron'>
+    </div>
+    <!-- End main content -->
+</body>
+
+</html>
