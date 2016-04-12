a6a697cb2f627d5e2b5ba5f234b886bb4422ac0f
nickydraz <nickydraz@gmail.com>
2016-04-12 17:18:23 -0500

    Added in 2 out of 4 showcase items

--- index.php
+++ index.php
@@ -27,9 +27,32 @@
     </header>
     <!-- End navbar -->
     <!-- Begin main content -->
-    <div class='text-center middle jumbotron'>
-        <h2>Featured Projects</h2>
-        <p class='lead'>A few select projects from various Computer Science courses</p>
+    <div class='container'>
+        <div class='text-center middle jumbotron'>
+            <h2>Featured Projects</h2>
+            <p class='lead'>A few select projects from various Computer Science courses</p>
+        </div>
+
+        <!--
+        Begin Showcase
+        These will be hardcoded for now,
+        but eventually will be randomly selected
+        from a pool
+        -->
+        <div class='row text-center'>
+            <div class='well col-md-4 col-md-offset-1'>
+                <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Vader Emporium Project' src='images/vader.png' /></a>
+                <p class='lead'>Vader's Emporium</p>
+                <p>An online storefront built with Javascript, PHP, and MySQL</p>
+            </div>
+            <div class='well col-md-4 col-md-offset-2'>
+                <a href='#'>
+                    <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Javascript Yahtzee Project' src='images/yahtzee.png' /></a>
+                    <p class='lead'>Multiplayer Yahtzee</p>
+                    <p>Multiplayer Yahtzee game built with Javascript. It's on the moon!</p>
+                </a>
+            </div>
+        </div>
     </div>
     <!-- End main content -->
 </body>
