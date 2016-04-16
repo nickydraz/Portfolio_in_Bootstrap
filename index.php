4a84d22348190ac38c566b8b8a2b5d54c6700181
nickydraz <nickydraz@gmail.com>
2016-04-16 17:58:28 -0500

    styled the navbar more

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class='active'>
+                    <li>
                         <a href='#'>Home</a>
                     </li>
                     <li>
