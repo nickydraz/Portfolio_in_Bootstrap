b6dfb783f8269aeb8f94c807c888907baee294c5
nickydraz <nickydraz@gmail.com>
2016-04-16 17:51:38 -0500

    Added a JQuery script source; corrected Javascript link

--- includes/style.php
+++ includes/style.php
@@ -3,6 +3,7 @@
 
 <!--Include needed styles -->
 <link rel="stylesheet" type="text/css" href="/boot/css/bootstrap.min.css">
-<link rel="stylesheet" type='text/css' href="/boot/js/bootstrap.min.js">
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="/boot/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="/boot/fonts/glyphicons-halflings-regular.svg">
 <link rel="stylesheet" type="text/css" href="/css/custom.css">
