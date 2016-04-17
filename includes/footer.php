879b63cd24d0379d0dac870495795b5849f4b51a
nickydraz <nickydraz@gmail.com>
2016-04-17 00:11:34 -0500

    Moved footer to separate php file; added include to index

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<!-- Begin footer -->
+<footer class='footer'>
+    <div class="container">
+        <div class='col-xs-4 col-md-1'>
+            <a target="_blank" href='https://www.linkedin.com/in/nicholas-drazenovic-0b5565b8'>
+                    LinkedIn
+                    </a>
+        </div>
+        <div class='col-xs-2 col-xs-offset-6 col-sm-3 col-sm-offset-5 col-md-1 col-md-offset-10'>
+            <a target="_blank" href='/documents/N_Drazenovic_Resume.pdf'>Resume</a>
+        </div>
+    </div>
+</footer>
+<!-- End footer -->
