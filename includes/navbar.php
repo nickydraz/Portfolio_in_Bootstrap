c3ef3f22a0b6b1812107f63f76ad94366591b512
nickydraz <nickydraz@gmail.com>
2016-04-17 00:08:33 -0500

    Moved navbar to a php file

---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+<!-- Begin header and navbar for page -->
+<nav class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class='navbar-brand hidden-xs' id="logo" href='/index.php'>Nicholas Drazenovic</a>
+            <a class='navbar-brand visible-xs' id="logo" href='/index.php'>N.D.</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href='/index.php'>Home</a>
+                </li>
+                <li>
+                    <a href='#'>Courses</a>
+                </li>
+                <li>
+                    <a href='#'>About</a>
+                </li>
+            </ul>
+
+        </div>
+        <!--/.navbar-collapse -->
+    </div>
+</nav>
