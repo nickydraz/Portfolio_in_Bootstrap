685af21e5464b832799a495a9e2e6ac941e35731
nickydraz <nickydraz@gmail.com>
2016-04-17 00:10:12 -0500

    Removed navbar, moved into separate php file and added include; Created footer for page

--- index.php
+++ index.php
@@ -1,71 +1,60 @@
 <html>
 
 <head>
+    <!-- Include the needed styles and other files -->
     <?php require 'includes/style.php'; ?>
         <title>Home | Nicholas Drazenovic</title>
 </head>
 
 <body>
-    <!-- Begin header and navbar for page -->
-    <nav class="navbar navbar-inverse navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class='navbar-brand hidden-xs' id="logo" href='/index.php'>Nicholas Drazenovic</a>
-                <a class='navbar-brand visible-xs' id="logo" href='/index.php'>N.D.</a>
+
+    <!-- Include the navbar -->
+    <?php require 'includes/navbar.php'; ?>
+
+        <!-- Begin main content -->
+        <div class='container'>
+            <div class='text-center jumbotron'>
+                <h2>Featured Projects</h2>
+                <p class='lead'>A few select projects from various Computer Science courses</p>
             </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href='/index.php'>Home</a>
-                    </li>
-                    <li>
-                        <a href='#'>Courses</a>
-                    </li>
-                    <li>
-                        <a href='#'>About</a>
-                    </li>
-                </ul>
 
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </nav>
-
-    <!-- Begin main content -->
-    <div class='container'>
-        <div class='text-center jumbotron'>
-            <h2>Featured Projects</h2>
-            <p class='lead'>A few select projects from various Computer Science courses</p>
-        </div>
-
-        <!--
+            <!--
         Begin Showcase
         These will be hardcoded for now,
         but eventually will be randomly selected
         from a pool
         -->
-        <div class='row text-center'>
-            <div class='well col-md-4 col-md-offset-1'>
-                <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Vader Emporium Project' src='images/vader.png' /></a>
-                <p class='lead'>Vader's Emporium</p>
-                <p>An online storefront built with Javascript, PHP, and MySQL</p>
-            </div>
-            <div class='well col-md-4 col-md-offset-2'>
-                <a href='#'>
-                    <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Javascript Yahtzee Project' src='images/yahtzee.png' /></a>
-                    <p class='lead'>Multiplayer Yahtzee</p>
-                    <p>Multiplayer Yahtzee game built with Javascript. It's on the moon!</p>
-                </a>
+            <div class='row text-center'>
+                <div class='well col-md-4 col-md-offset-1'>
+                    <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Vader Emporium Project' src='images/vader.png' /></a>
+                    <p class='lead'>Vader's Emporium</p>
+                    <p>An online storefront built with Javascript, PHP, and MySQL</p>
+                </div>
+                <div class='well col-md-4 col-md-offset-2'>
+                    <a href='#'>
+                        <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Javascript Yahtzee Project' src='images/yahtzee.png' /></a>
+                        <p class='lead'>Multiplayer Yahtzee</p>
+                        <p>Multiplayer Yahtzee game built with Javascript. It's on the moon!</p>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- End main content -->
+        <!-- End main content -->
+
+        <!-- Begin footer -->
+        <footer class='footer'>
+            <div class="container">
+                <div class='col-xs-4 col-md-1'>
+                    <a target="_blank" href='https://www.linkedin.com/in/nicholas-drazenovic-0b5565b8'>
+                    LinkedIn
+                    </a>
+                </div>
+                <div class='col-xs-2 col-xs-offset-6 col-sm-3 col-sm-offset-5 col-md-1 col-md-offset-10'>
+                    <a target="_blank" href='/documents/N_Drazenovic_Resume.pdf'>Resume</a>
+                </div>
+            </div>
+        </footer>
+        <!-- End footer -->
 </body>
 
 </html>
