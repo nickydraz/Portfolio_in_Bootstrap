2ee16995a0cdaf4a954b855c4182ec14373bdaa8
Nicholas Drazenovic <nickydraz@gmail.com>
2016-04-20 15:56:24 -0400

    Added course page and began adding pagination/content

---
+++ Courses/csc215.php
@@ -0,0 +1,14 @@
+<div class='row text-center'>
+    <div class='well col-md-4 col-md-offset-1'>
+        <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Vader Emporium Project' src='images/vader.png' /></a>
+        <p class='lead'>Vader's Emporium</p>
+        <p>An online storefront built with Javascript, PHP, and MySQL</p>
+    </div>
+    <div class='well col-md-4 col-md-offset-2'>
+        <a href='#'>
+            <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Javascript Yahtzee Project' src='images/yahtzee.png' /></a>
+            <p class='lead'>Multiplayer Yahtzee</p>
+            <p>Multiplayer Yahtzee game built with Javascript. It's on the moon!</p>
+        </a>
+    </div>
+</div>
---
+++ courses.php
@@ -0,0 +1,49 @@
+<?php $course;
+
+if ($_GET["course"] == null)
+{
+    $course="Courses/csc215.php";
+}
+else {
+   $course = "Courses/".$_GET["course"].".php"; 
+}
+
+
+
+
+?>
+<html>
+
+<head>
+    <!-- Include the needed styles and other files -->
+    <?php require 'includes/style.php'; ?>
+        <title>Courses | Nicholas Drazenovic</title>
+</head>
+
+<body>
+
+    <!-- Include the navbar -->
+    <?php require 'includes/navbar.php'; ?>
+    
+    <!-- Begin the main content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+                <ul class="pagination">
+                    <li><a href="courses.php?course=csc115">CSC 115</a></li>
+                    <li><a href="courses.php?course=csc215">CSC 215</a></li>
+                    <li><a href="courses.php?course=csc415">CSC 415</a></li>
+                    <li><a href="courses.php?course=csc425">CSC 425</a></li>
+                </ul>
+            </div>
+        </div><!--End row -->
+        
+        <!-- insert the proper course listing -->
+        <?php include $course;?>
+    </div>
+    
+
+
+</body>
+
+</html>
