3019d7768d9bedf782543c46e16d2b66e3d0bf27
Nicholas Drazenovic <nickydraz@gmail.com>
2016-05-11 11:43:52 -0500

    text changes

--- index.php
+++ index.php
@@ -28,13 +28,13 @@
                 <div class='well col-md-4 col-md-offset-1'>
                     <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Vader Emporium Project' src='images/vader.png' /></a>
                     <p class='lead'>Vader's Emporium</p>
-                    <p>An online storefront built with Javascript, PHP, and MySQL</p>
+                    <p>An online storefront built with Javascript, PHP, and MySQL.</p>
                 </div>
                 <div class='well col-md-4 col-md-offset-2'>
                     <a href='#'>
                         <a href='#'><img class='img-rounded img-responsive' alt='Screenshot of Javascript Yahtzee Project' src='images/yahtzee.png' /></a>
                         <p class='lead'>Multiplayer Yahtzee</p>
-                        <p>Multiplayer Yahtzee game built with Javascript. It's on the moon!</p>
+                        <p>Multiplayer Yahtzee game built with Javascript.</p>
                     </a>
                 </div>
             </div>
