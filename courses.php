8d0cb4caa18d78d24b155448a5b9d4f2b95bd266
Nicholas Drazenovic <nickydraz@gmail.com>
2016-05-11 14:40:56 -0500

    Fixed alignment of elements on Course page. Used to be off-center slightly, now fully centered. Will come back to this later to see if centered is what is actually desired

--- courses.php
+++ courses.php
@@ -14,12 +14,12 @@
         <!-- Begin the main content -->
         <div class="container">
             <div class="row">
-                <div class="col-xs-11">
+                <div class="col-xs-12">
                     <h4 class="text-center">Please select a course to view project samplings.</h4>
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-8 col-md-offset-2 hidden-xs">
+                <div class="col-md-12 hidden-xs text-center">
                     <ul class="pagination" id="courseListings">
                         <li id="CSC115" onclick="determineCourse('CSC115');"><a>CSC 115</a></li>
                         <li id="CSC215" onclick="determineCourse('CSC215');"><a>CSC 215</a></li>
