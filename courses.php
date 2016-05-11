5c891f1d3d76755945b46d4f7ab03a5037084463
Nicholas Drazenovic <nickydraz@gmail.com>
2016-05-11 14:32:58 -0500

    Added a dropdown menu that replaces the UL links on the Courses page on smaller screens, since this helps make the page more mobile-friendly.

--- courses.php
+++ courses.php
@@ -19,13 +19,23 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-8 col-xs-offset-2">
+                <div class="col-md-8 col-md-offset-2 hidden-xs">
                     <ul class="pagination" id="courseListings">
                         <li id="CSC115" onclick="determineCourse('CSC115');"><a>CSC 115</a></li>
                         <li id="CSC215" onclick="determineCourse('CSC215');"><a>CSC 215</a></li>
                         <li id="CSC415" onclick="determineCourse('CSC415');"><a>CSC 415</a></li>
                         <li id="CSC425" onclick="determineCourse('CSC425');"><a>CSC 425</a></li>
                     </ul>
+                </div>
+                <div class="col-xs-10 col-xs-offset-1 visible-xs-block visible-sm-block">
+                    <form class="form-group">
+                        <select class="form-control" onchange="determineCourse(this.value)">
+                            <option value="CSC115">CSC 115</option>
+                            <option value="CSC215">CSC 215</option>
+                            <option value="CSC415">CSC 415</option>
+                            <option value="CSC425">CSC 425</option>
+                        </select>
+                    </form>
                 </div>
             </div>
             <!--End row -->
