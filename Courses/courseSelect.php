9bd7d9fa8c073d1661401705c8e8104ec2262de4
Nicholas Drazenovic <nickydraz@gmail.com>
2016-05-30 17:36:38 -0500

    Changed formatting of Courses page:
    - If only one project, format the single project as before
    - Else, format as two projects per row
    
    These changes help keep the page looking consistent regardless of the number of courses.

--- Courses/courseSelect.php
+++ Courses/courseSelect.php
@@ -21,26 +21,16 @@
     $count = mysqli_num_rows($result);
     ?>
     <div class='row text-center'>
-        <?php if ($count == 3) {
-            $counter = 3;
-            while ($row = mysqli_fetch_assoc($result))
+        <div class="row">
+        <?php
+            //If there is only one project in the course,
+            //make it larger so it displays better on the page
+            if ($count == 1)
             {
-                $counter-- ?>
-            <div class='well col-md-2 col-md-offset-<?php echo max($counter, 1) ?> course-listing '>
-                <a href='<?php echo $row[ "Link"] ?>' target="_blank"><img class='img-rounded img-responsive' src='<?php echo $row["ImageLink"] ?>' /></a>
-                <p class='lead'>
-                    <?php echo $row["Title"]?>
-                </p>
-                <p>
-                    <?php echo $row["Description"] ?>
-                </p>
-            </div>
-            <?php } }
-            else if ($count == 2) {
-        $counter = 0;
-        while ($row = mysqli_fetch_assoc($result)) { $counter++; ?>
-                <div class='well col-md-4 col-md-offset-<?php echo min($counter, 3) ?> course-listing '>
-                    <a href='<?php echo $row[ "Link"] ?>' target="_blank"><img class='img-rounded img-responsive' src='<?php echo $row["ImageLink"] ?>' /></a>
+                $row = mysqli_fetch_assoc($result);
+                ?>
+                <div class='well col-md-6 col-md-offset-3 course-listing'>
+                    <a href='<?php echo $row["Link"] ?>' target="_blank"><img class='img-rounded img-responsive' src='<?php echo $row["ImageLink"] ?>' /></a>
                     <p class='lead'>
                         <?php echo $row["Title"]?>
                     </p>
@@ -48,9 +38,37 @@
                         <?php echo $row["Description"] ?>
                     </p>
                 </div>
-                <?php } } else if ($count == 1) { while ($row = mysqli_fetch_assoc($result)) { ?>
-                    <div class='well col-md-6 col-md-offset-3 course-listing'>
-                        <a href='<?php echo $row["Link"] ?>' target="_blank"><img class='img-rounded img-responsive' src='<?php echo $row["ImageLink"] ?>' /></a>
+            <?php }
+            else
+            {
+                //More than one project in the course,
+                //format as 2 projects per row
+
+                //Counter for the results
+                $counter = 0;
+                while ($counter < $count) {
+                    //Cycle through the results
+                    $row = mysqli_fetch_assoc($result);
+                    $counter++;
+
+                    //Logic for formatting
+                    $needRowStart = false;
+                    if ($counter % 2 != 0) {
+                        $offset = 1;
+                        $needRowStart = true;
+                    }
+                    else {
+                        $offset = 2;
+                        $needRowStart = false;
+                    }
+                    //Insert row div if needed
+                    if ($needRowStart)
+                    {
+                        echo '<div class="row">';
+                    }
+                    ?>
+                    <div class='well col-md-4 col-md-offset-<?php echo $offset; ?> course-listing '>
+                        <a href='<?php echo $row[ "Link"] ?>' target="_blank"><img class='img-rounded img-responsive' src='<?php echo $row["ImageLink"] ?>' /></a>
                         <p class='lead'>
                             <?php echo $row["Title"]?>
                         </p>
@@ -58,6 +76,13 @@
                             <?php echo $row["Description"] ?>
                         </p>
                     </div>
-                    <?php }
-}?>
+                    <?php
+                    //Insert closing row div tag if needed
+                    if (!$needRowStart) {
+                        echo '</div>';
+                    }
+                }//end while
+            }//end else
+         ?>
+        </div>
     </div>
