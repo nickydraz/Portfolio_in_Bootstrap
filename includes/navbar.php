570eee412b7169a8d158e34b38928a9d723b573e
Nicholas Drazenovic <nickydraz@gmail.com>
2016-05-30 23:23:15 -0500

    Added in new logo for the site.
    Added mobile logo, but it isn't displaying properly and needs to be fixed.

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,16 +1,16 @@
 <!-- Begin header and navbar for page -->
 <nav class="navbar navbar-inverse navbar-static-top">
     <div class="container-fluid">
-        <div class="navbar-header">
+        <a class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class='navbar-brand hidden-xs' id="logo" href='/index.php'>Nicholas Drazenovic</a>
-            <a class='navbar-brand visible-xs' id="logo" href='/index.php'>N.D.</a>
-        </div>
+            <a class='navbar-brand hidden-xs' id="logo" href='/index.php'><img class='img-responsive' src='/images/logo.png' /></a>
+            <a class='navbar-brand visible-xs' id="logo" href='/index.php'><img class='img-responsive' src='/images/mobile-logo.png' /></a>
+        </a>
         <div id="navbar" class="navbar-collapse collapse text-right">
             <ul class="nav navbar-nav navbar-right">
                 <li role="presentation" id="Home">
