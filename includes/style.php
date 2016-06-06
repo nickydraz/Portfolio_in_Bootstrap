82aefa733c78d229402192bee91af0ca7bfb6eb1
Nicholas Drazenovic <nickydraz@gmail.com>
2016-06-05 22:53:18 -0500

    Changed viewport meta tag

--- includes/style.php
+++ includes/style.php
@@ -1,7 +1,7 @@
 <!-- Meta -->
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name='viewport' content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta name="description" content="Portfolio site for Nicholas Drazenovic">
 <!-- Control the behavior of search engine crawling and indexing -->
 <meta name="robots" content="index,follow,noodp"><!-- All Search Engines -->
